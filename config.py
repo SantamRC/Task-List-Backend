d7d383e6de606686bafe2878168dca7d3ed6758d
SantamRC <santamdev404@gmail.com>
2025-05-30 01:39:47 +0530

    Debug for heroku deployment

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL").replace('postgres://', 'postgresql://', 1)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
